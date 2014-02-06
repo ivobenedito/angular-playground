1cb4c9e94486134381d300bb1f4cedb09e2cb877
ivobenedito <ivobenedito@gmail.com>
2014-02-06 01:03:38 +0000

    updated kontacts angular app

--- app/assets/javascripts/kontacts.js
+++ app/assets/javascripts/kontacts.js
@@ -1,5 +1,23 @@
 angular.module('kontacts', ['ngResource'])
-  .controller('KontactsController', function ($scope, $resource) {
-    var Contact = $resource('/api/contacts/:id');
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/contacts', {
+        controller: 'ContactsIndexController',
+        templateUrl: '/templates/kontacts/index.html'
+      })
+      .when('/contacts/:id', {
+        controller: 'ContactsEditController',
+        templateUrl: '/templates/kontacts/edit.html'
+      });
+
+    $locationProvider.html5Mode(true);
+  })
+  .factory('Contact', function ($resource) {
+    return $resource('/api/contacts/:id');
+  })
+  .controller('ContactsIndexController', function ($scope, Contact) {
     $scope.contacts = Contact.query();
+  })
+  .controller('ContactsEditController', function ($scope, $routeParams, Contact) {
+    $scope.contact = Contact.get({ id: $routeParams.id });
   });
