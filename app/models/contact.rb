83bbbf49ddb77ae8f2215340ff6b4058683edadd
ivobenedito <ivobenedito@gmail.com>
2014-02-06 00:16:19 +0000

    updated Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,8 @@ class Contact
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name,   type: String
-  field :email,  type: String
-  field :number, type: Integer
+  field :first_name, type: String
+  field :last_name,  type: String
+  field :email,      type: String
+  field :number,     type: Integer
 end
